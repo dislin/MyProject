f4be878925edc6cc18f4cb3b17b1b4e1bf5bc521
Leo <dislin@gmail.com>
2013-08-20 19:39:38 -0700

    Add confighelper and log setting

---
+++ MvcTest/MvcTest/Controllers/TestConfigController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommandLib.Helper;
+
+namespace MvcTest.Controllers
+{
+    public class TestConfigController : Controller
+    {
+        //
+        // GET: /TestConfig/
+
+        public ActionResult LogConfigTest()
+        {
+            ConfigHelper configLogSetting = new ConfigHelper(ConfigEnum.LogSetting, "Log4NetSetting");
+            ViewBag.limit = configLogSetting.GetValue("limit");
+            ViewBag.path = configLogSetting.GetValue("path");
+            return View(ViewBag);
+        }
+
+    }
+}
--- MvcTest/MvcTest/Controllers/TestDBController.cs
+++ MvcTest/MvcTest/Controllers/TestDBController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcTest.App_Code;
+using CommandLib.Helper;
+
 
 namespace MvcTest.Controllers
 {
