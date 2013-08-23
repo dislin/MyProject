cf0f6681b1405336a27041ca0cf94dc72521f91f
jaysharp <jaysharp@163.com>
2013-08-23 10:20:52 -0700

    added

---
+++ MvcTest/MvcTest/Controllers/TestDropDownController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTest.Controllers
+{
+    public class TestDropDownController : Controller
+    {
+        //
+        // GET: /TestDropDown/
+
+        public ActionResult TestDropDown()
+        {
+
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "LEO", Value = "LEO" });
+            items.Add(new SelectListItem { Text = "Yang", Value = "Yang" });
+            var dropdownList = new SelectList(items, "Text", "Value",
+                Request["cofoundersList"] == null ? "Yang" : Request["cofoundersList"]);
+            this.ViewData["cofoundersList"] = dropdownList;
+
+            YangLogger.SimpleLogger.Debug(dropdownList.SelectedValue + " is selected");
+
+            return View();
+        }
+
+    }
+}
