fe01ec95402427824d9d6b58cc3d86a42acf60db
Leo <dislin@gmail.com>
2013-08-24 13:38:21 +0800

    Update TestDropDownList

--- MvcTest/MvcTest/Controllers/TestDropDownController.cs
+++ MvcTest/MvcTest/Controllers/TestDropDownController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcTest.Models;
 
 namespace MvcTest.Controllers
 {
@@ -13,18 +14,41 @@ namespace MvcTest.Controllers
 
         public ActionResult TestDropDown()
         {
+            TestDropDownModel model = new TestDropDownModel();
+            List<TestMember> MemberList = new List<TestMember>();
+            MemberList.Add(new TestMember("Leo", 1));
+            MemberList.Add(new TestMember("YH", 2));
 
-            List<SelectListItem> items = new List<SelectListItem>();
-            items.Add(new SelectListItem { Text = "LEO", Value = "LEO" });
-            items.Add(new SelectListItem { Text = "Yang", Value = "Yang" });
-            var dropdownList = new SelectList(items, "Text", "Value",
-                Request["cofoundersList"] == null ? "Yang" : Request["cofoundersList"]);
-            this.ViewData["cofoundersList"] = dropdownList;
+            var oList = from m in MemberList
+                        select new
+                        {
+                            key = m.Id,
+                            name = m.Name
+                        };
+            model.selMemberList = new SelectList(oList, "key", "name");
 
-            YangLogger.SimpleLogger.Debug(dropdownList.SelectedValue + " is selected");
+            //List<SelectListItem> items = new List<SelectListItem>();
+            //items.Add(new SelectListItem { Text = "LEO", Value = "LEO" });
+            //items.Add(new SelectListItem { Text = "Yang", Value = "Yang" });
+            //var dropdownList = new SelectList(items, "Text", "Value",
+            //    Request["cofoundersList"] == null ? "Yang" : Request["cofoundersList"]);
+            //this.ViewData["cofoundersList"] = dropdownList;
 
-            return View();
+            //YangLogger.SimpleLogger.Debug(dropdownList.SelectedValue + " is selected");
+
+            return View(model);
         }
 
+    }
+
+    public class TestMember
+    {
+        public TestMember(string name, int id)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+        public string Name { get; set; }
+        public int Id { get; set; }
     }
 }
---
+++ MvcTest/MvcTest/Models/TestDropDownModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTest.Models
+{
+    public class TestDropDownModel
+    {
+        public int intMemberID { get; set; }
+        public SelectList selMemberList { get; set; }
+    }
+}
