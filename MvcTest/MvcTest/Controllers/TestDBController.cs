ed8586b36c6ed272c166b88c0f6d3a15dc9b0c9e
Leo <dislin@gmail.com>
2013-08-28 01:19:23 +0800

    Add DB function

--- MvcTest/MvcTest/Controllers/TestDBController.cs
+++ MvcTest/MvcTest/Controllers/TestDBController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using MvcTest.App_Code;
 using CommandLib.Helper;
+using System.Data.SqlClient;
+using System.Data;
 
 
 namespace MvcTest.Controllers
@@ -17,7 +19,15 @@ namespace MvcTest.Controllers
         public ActionResult TestDB1()
         {
             ConfigHelper config = new ConfigHelper(ConfigEnum.Database, "MainDB");
-            ViewBag.configValue = config.GetValue();
+            SqlConnection oSqlCn = new SqlConnection();
+            oSqlCn.ConnectionString = config.GetValue();
+            SqlCommand oCmd = new SqlCommand("role_GetAllRoleData", oSqlCn);
+            oCmd.CommandType = CommandType.StoredProcedure;
+            oSqlCn.Open();
+            SqlDataReader oDr = oCmd.ExecuteReader(CommandBehavior.CloseConnection);
+            oCmd.Dispose();
+            oSqlCn.Dispose();
+            //ViewBag.configValue = config.GetValue();
             return View(ViewBag);
         }
 
---
+++ MvcTest/MvcTest/Models/TestRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcTest.Controllers;
+using MvcTest.App_Code.Enum;
+
+namespace MvcTest.Models
+{
+    public class TestRole
+    {
+        public int idnum {get; set;}
+        public string name  {get; set;}
+        public CommonEnum.RoleStatusEnum status { get; set; }
+        public CommonEnum.RoleDeleteEnum isdelete { get; set; }
+        public string permission  {get; set;}
+        public string creater  {get; set;}
+        public string createdate  {get; set;}
+        public string modifier  {get; set;}
+        public string modifydate  {get; set;}
+    }
+}
