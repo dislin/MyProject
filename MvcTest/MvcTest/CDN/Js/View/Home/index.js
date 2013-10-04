6f35adb98c4f5b9791db9a680f9b22dbbe5c5db1
Leo Lin <dislin@gmail.com>
2013-10-04 19:06:32 +0800

    Add location feature

---
+++ MvcTest/MvcTest/CDN/Js/View/Home/index.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../../Lib/jquery-1.10.2.js" />
+/// <reference path="../../Lib/jquery.extend.js" />
+/// <reference path="../../Lib/jStorage.js" />
+/// <reference path="../../Lib/json2.js" />
+/// <reference path="../../Lib/knockout-2.3.0.js" />
+/*global alert,$,Enumerable,console,dialog,l,document,
+location,Utility,setTimeout,jQuery,require,window,arguments,navigator*/
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showLocation);
+    } else {
+        alert("Your browser not support location.")
+    }
+};
+
+function showLocation(position) {
+    var latitude = position.coords.latitude,
+        longitude = position.coords.longitude;
+    $("#location").text("you are at Latitude:" + latitude + ", Longitude:" + longitude);
+};
+$(function () {
+    getLocation();
+});
