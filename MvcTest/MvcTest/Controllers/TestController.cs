c4f7cfaf4b0ef7f1afd0ad03159eea8316f2c367
Leo Lin <dislin@gmail.com>
2013-10-28 19:21:23 +0800

    Add new Test

--- MvcTest/MvcTest/Controllers/TestController.cs
+++ MvcTest/MvcTest/Controllers/TestController.cs
@@ -18,40 +18,7 @@ namespace MvcTest.Controllers
 
         public ActionResult WebSiteParserTest()
         {
-            string txtUrl = @"https://www.acttab.com.au/keno/results",
-                txtContent = string.Empty;
-            HttpWebResponse response = null;
-            StreamReader readStream = null;
-
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(txtUrl);
-                request.MaximumAutomaticRedirections = 1;
-                request.MaximumResponseHeadersLength = 4;
-                request.Timeout = 30 * 1000; // if within x seconds still cannot scrape result, then give up
-
-                // Set credentials to use for this request.
-                request.Credentials = CredentialCache.DefaultCredentials;
-
-                response = (HttpWebResponse)request.GetResponse();
-                Stream receiveStream = response.GetResponseStream();
-                readStream = new StreamReader(receiveStream, Encoding.UTF8);
-                txtContent = readStream.ReadToEnd();
-            }
-            catch (Exception ex) {
-            }
-            finally
-            {
-                if (response != null)
-                {
-                    response.Close();
-                }
-
-                if (readStream != null)
-                {
-                    readStream.Close();
-                }
-            }
+            string txtContent = getHTML(@"https://www.acttab.com.au/keno/results");
 
             string[] arrContent = txtContent.Split(new string[] { "<tbody>" }, StringSplitOptions.None);
             txtContent = arrContent[1];
@@ -94,10 +61,82 @@ namespace MvcTest.Controllers
 
             for (int num = 0; num < num1; num += 1)
             {
-                ViewBag.Content += dicDrawNo[num] + ":" + dicResult[num] + ";";
+                ViewBag.Content += "<div>" + dicDrawNo[num] + ": " + dicResult[num] + "</div>";
             }
             return View();
         }
 
+        public ActionResult WebSiteParserTest2() {
+            string txtContent = getHTML(@"http://www.wclc.com/winning-numbers/keno.htm?drawNum=854712");
+            string[] arrContent = txtContent.Split(new string[] { "<table class=\"kenoTable\">" }, StringSplitOptions.None);
+            txtContent = arrContent[1];
+            arrContent = txtContent.Split(new string[] { "</table>" }, StringSplitOptions.None);
+            txtContent = arrContent[0];
+            arrContent = txtContent.Split(new string[] { "</tr>" }, StringSplitOptions.None);
+            txtContent = arrContent[1];
+
+            txtContent = txtContent.Replace("<tr>", "");
+            txtContent = txtContent.Replace(" ", "");
+            txtContent = txtContent.Replace("class=\"kenoDrawNumber\"", "");
+            txtContent = txtContent.Replace("<", "_{");
+            txtContent = txtContent.Replace(">", "}_");
+            txtContent = txtContent.Replace("\r", "");
+            txtContent = txtContent.Replace("\n", "");
+            txtContent = txtContent.Replace("\t", "");
+            txtContent = txtContent.Replace("{/td}_", "{/td}_ ");
+
+            string txtPattern = @"\b_{td}_(?<num>\S*)_{/td}_\b", tmpString="";
+
+            MatchCollection matches = Regex.Matches(txtContent, txtPattern);
+            foreach (Match match in matches)
+            {
+                GroupCollection groups = match.Groups;
+                tmpString += groups["num"].Value + ",";
+                
+            }
+            ViewBag.Content = tmpString.Substring(0, tmpString.Length - 1);
+            //ViewBag.Content = txtContent;
+            return View();
+        }
+
+        internal string getHTML(string Url) {
+            string txtUrl = Url,
+                txtContent = string.Empty;
+            HttpWebResponse response = null;
+            StreamReader readStream = null;
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(txtUrl);
+                request.MaximumAutomaticRedirections = 1;
+                request.MaximumResponseHeadersLength = 4;
+                request.Timeout = 30 * 1000; // if within x seconds still cannot scrape result, then give up
+
+                // Set credentials to use for this request.
+                request.Credentials = CredentialCache.DefaultCredentials;
+
+                response = (HttpWebResponse)request.GetResponse();
+                Stream receiveStream = response.GetResponseStream();
+                readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                txtContent = readStream.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                }
+
+                if (readStream != null)
+                {
+                    readStream.Close();
+                }
+            }
+
+            return txtContent;
+        }
     }
 }
